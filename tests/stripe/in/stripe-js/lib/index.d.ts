e3d39fbe69e66a391166cb9c4f00cb49b79ce1f9
Jason Pickens <jasonpickensnz@gmail.com>
2024-07-26 17:08:41 +1200

    Add normal looking import test

--- tests/stripe/in/stripe-js/lib/index.d.ts
+++ tests/stripe/in/stripe-js/lib/index.d.ts
@@ -2,3 +2,13 @@
 
 // import {loadStripe} from '../dist';
 // export {loadStripe};
+
+import {StripeConstructor} from "../dist/stripe-js";
+
+declare global {
+  interface Window {
+    // Stripe.js must be loaded directly from https://js.stripe.com/v3, which
+    // places a `Stripe` object on the window
+    Stripe?: StripeConstructor;
+  }
+}
